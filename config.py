7a0f041cf34f94cb0d0aedc35c1f95f3f6317f1d
SastaCarry <92705257+SastaCarry@users.noreply.github.com>
2024-08-01 23:01:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7382044675:AAFdeN5FYQDZU9wakpHZ3_oXSIRsjChb_1A")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("BaggaOG")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1122083582")
 
 
 
